a5e5793d962e93655e209a0e5e0375354dc46c8d
Moritz Wolter <v0lta@users.noreply.github.com>
2023-02-27 11:23:50 +0100

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -13,9 +13,8 @@ def run_test(session):
 @nox.session(name="lint")
 def lint(session):
     """Check code conventions."""
-    session.install("flake8==4.0.1")
+    session.install("flake8")
     session.install(
-        "flake8-colors",
         "flake8-black",
         "flake8-docstrings",
         "flake8-bugbear",
@@ -24,8 +23,8 @@ def lint(session):
         "pydocstyle",
         "darglint",
     )
-    session.install("flake8-bandit==2.1.2", "bandit==1.7.2")
     session.run("flake8", "src", "tests", "noxfile.py")
+
 
 
 @nox.session(name="typing")
